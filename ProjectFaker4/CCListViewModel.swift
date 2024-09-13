be2ce4fa62a6c771a3502360d51fc78551e20b8e
Ewan <ewan.urbanc@gmail.com>
2024-09-13 17:37:46 +0200

    V6 ca marche un peu mieux, on peut presque voir les cb une a une par id

--- ProjectFaker4/CCListViewModel.swift
+++ ProjectFaker4/CCListViewModel.swift
@@ -7,27 +7,25 @@
 
 import Foundation
 import Alamofire
-/*
-class CCListViewModel: ObservableObject {
-    @Published var card: [PersonIdentifiable] = []
+import SwiftUI
 
-    func fetchPeople() {
+class CCListViewModel: ObservableObject {
+    @Published var card : CreditCard?
+        
+    func fetchData(type: String){
         let urlString = "https://fakerapi.it/api/v2/creditCards"
         AF.request(urlString)
-            .validate() // Pour valider les réponses HTTP 200..<300 et les erreurs de contenu
-            .responseDecodable(of: CCResponse.self) { response in // Notez le type [User].self ici
+            .validate()
+            .responseDecodable(of: CCResponse.self) {response in
                 switch response.result {
-                case .success(let ccResponse):
-                    let cc = ccResponse.data
-                    var index : Int = 0
-                    for creditcard in cc {
-                        self.card.append(PersonIdentifiable(idList: cc.idList, id: cc.id, firstname: cc.firstname, lastname: cc.lastname, address: cc.address, image: cc.image, creditcard: cc.creditcard)
-                        index += 1
-                    }
+                case .success(let CCResponse):
+                    let cb = CCResponse.data
+                    self.card = cb[0]
+                    
                 case .failure(let error):
                     print("Erreur : \(error)")
                 }
             }
     }
 }
-*/
+
