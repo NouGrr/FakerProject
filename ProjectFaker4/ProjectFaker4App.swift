30a387847d053da71d0337a47577df35e6002150
Ewan <ewan.urbanc@gmail.com>
2024-09-13 13:47:14 +0200

    Initial Commit

---
+++ ProjectFaker4/ProjectFaker4App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectFaker4App.swift
+//  ProjectFaker4
+//
+//  Created by Quentin Courrier on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectFaker4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectFaker4UITests/ProjectFaker4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProjectFaker4UITestsLaunchTests.swift
+//  ProjectFaker4UITests
+//
+//  Created by Quentin Courrier on 9/13/24.
+//
+
+import XCTest
+
+final class ProjectFaker4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
