f12c772c778aa92deb1f882744d63f7beba256fc
Ewan <ewan.urbanc@gmail.com>
2024-09-27 11:58:56 +0200

    test commit 2

--- ProjectFaker4/CCListView.swift
+++ ProjectFaker4/CCListView.swift
@@ -33,7 +33,7 @@ struct CreditCardListView : View {
                     Text("Chargement dses informations")
                 }
             }
-            .navigationTitle("Credit Cards de merde")
+            .navigationTitle("Credit Cards")
             .onAppear {
                 viewModel.fetchData(type: "visa")
             }
